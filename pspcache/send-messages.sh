a5a4059940d06e9cc25f9c94e249e4d1e8c84324
flyisland <flyisland@gmail.com>
2022-04-01 08:58:07 +0800

    same message body in the same loop

--- pspcache/send-messages.sh
+++ pspcache/send-messages.sh
@@ -3,11 +3,12 @@
 currencies=("CNY/USD" "CNY/JPY" "CNY/EUR" "USD/EUR" "USD/GBP" "USD/CAD" "USD/CNY" "USD/JPY")
 
 while true; do 
+    msgbody=$(date +'%Y-%m-%d-%H:%M:%S')
     for match in "${currencies[@]}"
     do
-        msgbody=$(date +'%Y-%m-%d-%H:%M:%S')
         echo "Sending '${msgbody}' to topic 'fx/rt/${match}'"
         curl -X POST -d "${msgbody}" http://localhost:9000/fx/rt/${match} -H "content-type: text/plain"
-        sleep 1
     done
+    echo
+    sleep 1
 done
